44a502cd34a252d409eafa9c7bc0716319931c79
Ankaj <138543932+Ankaj22@users.noreply.github.com>
2026-09-07 18:04:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(__dirname));
 
 app.get("/api/status", (req, res) => {
   res.json({
     app: "X-Dark",
     status: "online"
   });
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
